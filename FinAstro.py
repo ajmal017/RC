5d20c9e464eaba8c30daf5cadcc77c83d5f798c8
Rushi Chhapia <rushic@users.noreply.github.com>
2018-12-05 16:38:13 +0000

    Update FinAstro.py

--- FinAstro.py
+++ FinAstro.py
@@ -14,7 +14,7 @@ def get_degrees(x):
 
 def get_aspect(x):
     aspect = ''
-    orb = 10
+    orb = 5
     if (120 + orb >= x['Degrees']) and (120 - orb <= x['Degrees']):
         aspect = 'TR'
     elif (60 + orb >= x['Degrees']) and (60 - orb <= x['Degrees']):
@@ -75,48 +75,48 @@ def create_df_old(ticker, weeks_of_history):
 def label_point(x, y, val, ax):
     a = pd.concat({'x': x, 'y': y, 'val': val}, axis=1)
     for i, point in a.iterrows():
-        ax.text(point['x'], point['y'], str(point['val']), fontsize=6)
+        aspect = str(point['val'])
+        if aspect == 'nan':
+            aspect = ''
+        ax.text(point['x'], point['y'], aspect, fontsize=6)
 
 
 def plot_astro_stock(df):
-    fig = plt.figure()
-    #ax1 = fig.add_subplot(2, 1, 1)
-    #ax1.plot(df.index, df['Close'], '-', color='b')
-    ax1 = fig.add_subplot(211,  ylabel='Value', facecolor='0.9')
-    df['Close'].plot(ax=ax1, color='y', lw=0.5)
-    label_point(df['cob'].astype(str), df['Close'].astype(float), df['Aspect'], ax1)
 
-    ax2 = fig.add_subplot(212,  ylabel='Degrees', facecolor='0.9')
-    df['Degrees'].plot(ax=ax2, color='b', lw=0.5)            
+    # fig = plt.figure()
+    f, (ax1) = plt.subplots(1, sharex=False, sharey=False)
+    x_t = pd.to_datetime(df.index.values)
+
+    ax1.plot(x_t, df['Close'], label='', color='black')
+    # ax2.plot(x_t, df['Degrees'], label='', color='blue')
+    label_point(df['cob'].astype(str), df['Close'].astype(float), df['Aspect'], ax1)
     plt.show()
 
 
-def add_detail_to_ephemeris():
-    df = pd.read_csv('Ephemeris2024.csv')
-    df['Date'] = pd.to_datetime(df['Date']).dt.strftime('%Y-%m-%d')
-    df['Degrees'] = df.apply(get_degrees, axis=1)
-    df['Aspect'] = df.apply(get_aspect, axis=1)
-    df['Sign'] = df.apply(get_sign, axis=1)
-    df['cob'] = df['Date']
-    df.to_csv('ephemeris_detail.csv', index=False)
-
-
-def create_df():
+def create_df(ticker = 'S&P500'):
     # ticker = 'NASDAQ_MSFT'
-    ticker = 'S&P500'
-    df_ticker = pd.read_csv('{}{}.csv'.format(cp.dirs['PriceHistories'], ticker))
+
+    df_ticker = pd.read_csv('{}{}.csv'.format(cp.dirs['PriceHistories'], ticker, index_col=True))
     df_ticker['Change_pct_' + ticker] = df_ticker['Close_' + ticker].pct_change()
     df_ticker = df_ticker.set_index('Date')
 
-    df_eph = pd.read_csv('ephemeris_detail.csv')
+    df_eph = pd.read_csv('Ephemeris2024.csv')
+    df_eph['Date'] = pd.to_datetime(df_eph['Date']).dt.strftime('%Y-%m-%d')
+    df_eph['Degrees'] = df_eph.apply(get_degrees, axis=1)
+    df_eph['Aspect'] = df_eph.apply(get_aspect, axis=1)
+    df_eph['Sign'] = df_eph.apply(get_sign, axis=1)
+    df_eph['cob'] = df_eph['Date']
     df_eph = df_eph.set_index('Date')
+    df_eph = df_eph[df_eph.index < '2018-13-31']
 
-    df = df_ticker.join(df_eph, how='inner', on='Date')
+    # df = df_ticker.join(df_eph, how='inner')
+    df = df_eph.join(df_ticker, how='outer')
     df.to_csv('finastro.csv')
     df['Close'] = df['Close_' + ticker]
-    plot_astro_stock(df)
-
-    #print(df.head)
+    # plot_astro_stock(df)
+    print(type(df.index))
+    df = df[df.index > '2018-11-01']
+    print(df.tail)
 
 
 if __name__ == "__main__":
